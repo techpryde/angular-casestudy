0f9e0e82c380ed7c5a1a5330f18a2ac8a9fe5c9d
Pardeep <pk.raswant@gmail.com>
2018-06-13 10:45:26 +0530

    changes in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works12!';
   @Input() imgPath:string = "../../src/res/";
   imgFileName:string = "logo.png";
 }
